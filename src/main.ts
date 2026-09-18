717f0068c2ca2c878c7bb961c468420147d843f8
Moaz Ali <moazj048@gmail.com>
2026-09-19 02:10:40 +0500

    specify NestExpressApplication type for NestFactory and set trust proxy

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import {NestExpressApplication} from "@nestjs/platform-express";
 import { ThrottlerExceptionFilter } from './common/filters/throttler-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+ const app = await NestFactory.create<NestExpressApplication>(AppModule);
+app.set('trust proxy', 1);
   const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('v1');
